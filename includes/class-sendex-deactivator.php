ccf6d912242fc3776c6257b5c1328557ee506696
irenebogues <irenebogues@gmail.com>
2019-04-22 14:39:07 -0400

    updating plugin scripts

---
+++ includes/class-sendex-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://irenebogues.com
+ * @since      1.0.0
+ *
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ * @author     Irene Bogues <irenebogues@gmail.com>
+ */
+class Sendex_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
